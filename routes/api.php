cd695c178234d0b93ab8c83854ad647e79b9e515
xHacka <pvpgamerpro457@gmail.com>
2023-10-17 23:52:27 +0400

    Make API Work! Woop Woop

---
+++ routes/api.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+Route::get   ('posts',      [PostController::class, 'index' ]);
+Route::get   ('posts/{id}', [PostController::class, 'view'  ]);
+Route::post  ('posts',      [PostController::class, 'store' ]);
+Route::put   ('posts/{id}', [PostController::class, 'update']);
+Route::delete('posts/{id}', [PostController::class, 'delete']);
